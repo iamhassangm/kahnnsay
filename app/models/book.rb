ed3268df9f63288419433b7543c2bfdc7f41dc6f
Hassan Gul <wedesignerdxb@gmail.com>
2022-09-29 12:39:52 +0500

    issue #3 fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,9 +4,8 @@ class Book < ApplicationRecord
   
   def self.find_with_libraries(id)
     book = find(id)
-    libraries_name = book.libraries.map{|lib| lib.name}
     result = book.attributes
-    result["libraries"] = libraries_name
+    result["libraries"] = book.libraries
     result
   end
 end
